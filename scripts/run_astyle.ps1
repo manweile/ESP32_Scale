cbd6b8f32b7d4bdbe2f8b4ca4649bd0e970d04f6
manweile <manweile@ualberta.ca>
2026-05-25 17:20:41 -0600

    astyle update

--- scripts/run_astyle.ps1
+++ scripts/run_astyle.ps1
@@ -1,5 +1,5 @@
 $files = Get-ChildItem -Path 'D:\MyArduino\Projects\ESP32_Scale\PropaneScale' -Recurse -Include *.c,*.cpp,*.h,*.hpp,*.ino
 foreach ($f in $files) {
     Write-Output "Formatting: $($f.FullName)"
-    & 'C:\AStyle\astyle.exe' --options='D:\MyArduino\Projects\ESP32_Scale\.astylerc' $f.FullName
+    & 'C:\AStyle\astyle.exe' --options='D:\MyArduino\Projects\ESP32_Scale\.astylerc' --suffix=none $f.FullName
 }
